c65f1324a58d0b87b681d3620c9181f1104f2d2b
Jeff Brown Tech <jeff@jeffbrown.tech>
2022-07-14 06:23:14 -0700

    policy assignment

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source     = "hashicorp/azurerm"
-      verversion = "3.13.0"
+      source  = "hashicorp/azurerm"
+      version = "3.13.0"
     }
   }
 }
@@ -11,56 +11,65 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_policy_definition" "name" {
+resource "azurerm_policy_definition" "sa-naming-convention" {
   name         = "StorageAccountNamingConvention"
   display_name = "Storage Accounts should follow naming convention"
   mode         = "Indexed"
   policy_type  = "Custom"
 
-  policy_rule = <<POLICY_RULE
-{
-    "if": {
-        "allOf": [
-            {
-                "field": "type",
-                "equals": "Microsoft.Storage/storageAccounts"
-            },
-            {
-                "field": "name",
-                "notmatch": "[parameters('namingPattern')]"
-            }
-        ]
-    },
-    "then": {
-        "effect": "[parameters('effectAction')]
+  metadata = jsonencode({
+    "version" : "1.0.0",
+    "category" : "Storage"
     }
-}
-POLICY_RULE
+  )
 
-  parameters = <<PARAMETERS
-{
-    "namingPattern": {
-        "type": "String",
-        "metadata": {
-            "displayName": "Naming Pattern"
-            "description": "Storage Account naming pattern. Using ? for letters, # for numbers."
-        }
-        "defaultValue": "4sysops???####"
+  parameters = jsonencode({
+    "namingPattern" : {
+      "type" : "String",
+      "metadata" : {
+        "displayName" : "Naming Pattern",
+        "description" : "Storage Account naming pattern. Using ? for letters, # for numbers."
+      },
+      "defaultValue" : "4sysops???####"
     },
 
-    "effectAction": {
-        "type": "String",
-        "metadata": {
-            "displayName": "Effect Action"
-            "description": "The effect action for the policy (Audit or Deny)."
+    "effectAction" : {
+      "type" : "String",
+      "metadata" : {
+        "displayName" : "Effect Action",
+        "description" : "The effect action for the policy (Audit or Deny)."
+      },
+      "allowedValues" : [
+        "Audit",
+        "Deny"
+      ],
+      "defaultValue" : "Audit"
+    }
+    }
+  )
+
+  policy_rule = jsonencode({
+    "if" : {
+      "allOf" : [
+        {
+          "field" : "type",
+          "equals" : "Microsoft.Storage/storageAccounts"
         },
-        "allowedValues": [
-            "Audit",
-            "Deny"
-        ]
-        "defaultValue": "Audit"
+        {
+          "field" : "name",
+          "notmatch" : "[parameters('namingPattern')]"
+        }
+      ]
+    },
+    "then" : {
+      "effect" : "[parameters('effectAction')]"
     }
+    }
+  )
 }
-PARAMETERS
 
+resource "azurerm_subscription_policy_assignment" "demo" {
+  name                 = "Storage Accounts should following naming convention (demo subscription)"
+  policy_definition_id = azurerm_policy_definition.sa-naming-convention.id
+  subscription_id      = "/subscriptions/00000000-0000-0000-000000000000"
 }
